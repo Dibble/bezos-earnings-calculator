b9f392f7357cf75adfa6710715f62ead9511f239
Dibble <james.cotterell@newvoicemedia.com>
2020-03-16 11:06:20 +0000

    Display whole seconds for user salary, inline user salary input

--- src/App.svelte
+++ src/App.svelte
@@ -14,7 +14,7 @@
 	$: bezosEarnedSinceLoad = formatCurrency(millisecondsSincePageLoad * bezosPerMillisecond)
 
 	let userSalary = 50000
-	$: secondsToEarn = userSalary / (bezosPerMillisecond * 1000)
+	$: secondsToEarn = Math.floor(userSalary / (bezosPerMillisecond * 1000))
 
 	const interval = setInterval(() => millisecondsSincePageLoad += 250, 250)
 	onDestroy(() => clearInterval(interval))
@@ -23,7 +23,6 @@
 <h1>Bezos Earnings Calculator</h1>
 <p>In 2018, Jeff Bezos earned $78.5 Billion. This is equivalent to $215 Million per day, or $2,489 per second.</p>
 
-<input type="number" bind:value={userSalary}>
-<p>It would take Jeff Bezos {secondsToEarn.toFixed(0)} seconds to earn your annual salary.</p>
+<p>It would take Jeff Bezos {secondsToEarn} seconds to earn your annual salary of <input type="number" bind:value={userSalary}></p>
 
 <p>In the {secondsSincePageLoad} seconds this page has been open, Jeff Bezos has earned {bezosEarnedSinceLoad}</p>
