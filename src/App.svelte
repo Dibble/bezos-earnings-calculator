05da78d1e2de2c74c2970e24fd3defcf5734b173
Dibble <james.cotterell@newvoicemedia.com>
2020-03-16 12:00:23 +0000

    Format time into readable text

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,39 @@
 <script>
 	import { onDestroy } from 'svelte'
 
-	const formatCurrency = (raw) => new Intl.NumberFormat('en-US', {
-			style: 'currency',
-			currency: selectedCurrency,
-			minimumIntegerDigits: 1,
-			maximumFractionDigits: 2
-		}).format(raw)
-
-	const bezosPerMillisecond = 2.489
 	let millisecondsSincePageLoad = 0
 	$: secondsSincePageLoad = (millisecondsSincePageLoad / 1000).toFixed(0)
+	const interval = setInterval(() => millisecondsSincePageLoad += 250, 250)
+	onDestroy(() => clearInterval(interval))
+
+	const bezosPerMillisecond = 2.489
 	$: bezosEarnedSinceLoad = formatCurrency(millisecondsSincePageLoad * bezosPerMillisecond * rates[selectedCurrency])
 
 	let userSalary = 50000
 	$: secondsToEarn = Math.floor((userSalary / rates[selectedCurrency]) / (bezosPerMillisecond * 1000))
-
-	const interval = setInterval(() => millisecondsSincePageLoad += 250, 250)
-	onDestroy(() => clearInterval(interval))
 
 	let selectedCurrency = 'USD'
 	let rates = {
 		'USD': 1
 	}
 
-	// async code
+	const formatCurrency = (raw) => new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: selectedCurrency,
+			minimumIntegerDigits: 1,
+			maximumFractionDigits: 2
+		}).format(raw)
+	
+	const formatTime = (totalSeconds) => {
+		const years = Math.floor(totalSeconds / (60*60*24*365))
+		const days = Math.floor(totalSeconds % (60*60*24*365) / (60*60*24))
+		const hours = Math.floor((totalSeconds % (60*60*24)) / (60*60))
+		const minutes = Math.floor((totalSeconds % (60*60)) / 60)
+		const seconds = totalSeconds % 60
+
+		return `${years > 0 ? `${years} year${years > 1 ? 's' : ''} ` : ''}${days > 0 ? `${days} day${days > 1 ? 's' : ''} ` : ''}${hours > 0 ? `${hours} hour${hours > 1 ? 's' : ''} ` : ''}${minutes > 0 ? `${minutes} minute${minutes > 1 ? 's' : ''} ` : ''}${seconds === 1 ? '1 second' : `${seconds} seconds`}`
+	}
+
 	async function loadExchangeRates () {
 		const exchangeRatesResult = await fetch('https://api.exchangeratesapi.io/latest?base=USD')
 		if (exchangeRatesResult.status !== 200) {
@@ -36,16 +45,15 @@
 		rates = exchangeRates.rates
 	}
 	loadExchangeRates()
-
 </script>
 
 <h1>Bezos Earnings Calculator</h1>
 <p>In 2018, Jeff Bezos earned $78.5 Billion. This is equivalent to $215 Million per day, or $2,489 per second.</p>
 
-<p>It would take Jeff Bezos {secondsToEarn} seconds to earn your annual salary of <input type="number" bind:value={userSalary} /><select bind:value={selectedCurrency} on:change="{e => selectedCurrency = e.target.value}">
+<p>It would take Jeff Bezos {formatTime(secondsToEarn)} to earn your annual salary of <input type="number" bind:value={userSalary} step=2500 /><select bind:value={selectedCurrency} on:change="{e => selectedCurrency = e.target.value}">
 	{#each Object.keys(rates).sort() as currency (currency)}
 		<option value={currency}>{currency}</option>
 	{/each}
 </select></p>
 
-<p>In the {secondsSincePageLoad} {secondsSincePageLoad === '1' ? 'second' : 'seconds'} this page has been open, Jeff Bezos has earned {bezosEarnedSinceLoad}</p>
+<p>In the {formatTime(secondsSincePageLoad)} this page has been open, Jeff Bezos has earned {bezosEarnedSinceLoad}</p>
