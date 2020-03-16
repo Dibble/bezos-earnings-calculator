a94de7a5144b9244e8d8764f77284b555234f974
Dibble <james.cotterell@newvoicemedia.com>
2020-03-16 11:46:35 +0000

    Fetch exchange rates and allow user to select currency

--- src/App.svelte
+++ src/App.svelte
@@ -3,26 +3,49 @@
 
 	const formatCurrency = (raw) => new Intl.NumberFormat('en-US', {
 			style: 'currency',
-			currency: 'USD',
+			currency: selectedCurrency,
 			minimumIntegerDigits: 1,
 			maximumFractionDigits: 2
 		}).format(raw)
 
 	const bezosPerMillisecond = 2.489
 	let millisecondsSincePageLoad = 0
-	$: secondsSincePageLoad = (millisecondsSincePageLoad / 1000).toFixed(1)
-	$: bezosEarnedSinceLoad = formatCurrency(millisecondsSincePageLoad * bezosPerMillisecond)
+	$: secondsSincePageLoad = (millisecondsSincePageLoad / 1000).toFixed(0)
+	$: bezosEarnedSinceLoad = formatCurrency(millisecondsSincePageLoad * bezosPerMillisecond * rates[selectedCurrency])
 
 	let userSalary = 50000
-	$: secondsToEarn = Math.floor(userSalary / (bezosPerMillisecond * 1000))
+	$: secondsToEarn = Math.floor((userSalary / rates[selectedCurrency]) / (bezosPerMillisecond * 1000))
 
 	const interval = setInterval(() => millisecondsSincePageLoad += 250, 250)
 	onDestroy(() => clearInterval(interval))
+
+	let selectedCurrency = 'USD'
+	let rates = {
+		'USD': 1
+	}
+
+	// async code
+	async function loadExchangeRates () {
+		const exchangeRatesResult = await fetch('https://api.exchangeratesapi.io/latest?base=USD')
+		if (exchangeRatesResult.status !== 200) {
+			console.log('failed to load exchange rates', exchangeRatesResult.status, await exchangeRatesResult.text())
+			return
+		}
+
+		const exchangeRates = await exchangeRatesResult.json()
+		rates = exchangeRates.rates
+	}
+	loadExchangeRates()
+
 </script>
 
 <h1>Bezos Earnings Calculator</h1>
 <p>In 2018, Jeff Bezos earned $78.5 Billion. This is equivalent to $215 Million per day, or $2,489 per second.</p>
 
-<p>It would take Jeff Bezos {secondsToEarn} seconds to earn your annual salary of <input type="number" bind:value={userSalary}></p>
+<p>It would take Jeff Bezos {secondsToEarn} seconds to earn your annual salary of <input type="number" bind:value={userSalary} /><select bind:value={selectedCurrency} on:change="{e => selectedCurrency = e.target.value}">
+	{#each Object.keys(rates).sort() as currency (currency)}
+		<option value={currency}>{currency}</option>
+	{/each}
+</select></p>
 
-<p>In the {secondsSincePageLoad} seconds this page has been open, Jeff Bezos has earned {bezosEarnedSinceLoad}</p>
+<p>In the {secondsSincePageLoad} {secondsSincePageLoad === '1' ? 'second' : 'seconds'} this page has been open, Jeff Bezos has earned {bezosEarnedSinceLoad}</p>
